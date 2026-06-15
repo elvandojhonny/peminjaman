1fb6caeb441f2d1932079e60451f611ceeca74d3
Elvando J <elvandojhonny@gmail.com>
2026-06-15 14:55:30 +0800

    Update get_peminjaman.php

--- get_peminjaman.php
+++ get_peminjaman.php
@@ -3,29 +3,24 @@
 include 'koneksi.php';
 include 'auto_update_status.php';
 
-$data = array();
-
-$query = mysqli_query(
-    $koneksi,
-    "SELECT
+$stmt = $koneksi->prepare("
+    SELECT
         p.*,
         g.fakultas
 
-     FROM peminjaman p
+    FROM peminjaman p
 
-     LEFT JOIN ruang r
-     ON p.nama_ruang = r.nama_ruang
+    LEFT JOIN ruang r
+        ON p.nama_ruang = r.nama_ruang
 
-     LEFT JOIN gedung g
-     ON r.id_gedung = g.id
+    LEFT JOIN gedung g
+        ON r.id_gedung = g.id
 
-     ORDER BY p.id DESC"
-);
+    ORDER BY p.id DESC
+");
 
-while($row = mysqli_fetch_assoc($query)){
-    $data[] = $row;
-}
+$stmt->execute();
+
+$data = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 echo json_encode($data);
-
-?>
