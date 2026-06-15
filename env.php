eaff1e32cfcbdd36ee3702e82ff4da0b38d52bf5
Elvando J <elvandojhonny@gmail.com>
2026-06-15 13:57:31 +0800

    Create env.php

---
+++ env.php
@@ -0,0 +1,8 @@
+<?php
+
+echo "HOST = " . getenv('MYSQLHOST') . "<br>";
+echo "PORT = " . getenv('MYSQLPORT') . "<br>";
+echo "USER = " . getenv('MYSQLUSER') . "<br>";
+echo "DB = " . getenv('MYSQLDATABASE') . "<br>";
+
+echo "<hr>PHP OK";
