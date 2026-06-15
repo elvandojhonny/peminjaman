28d45b5098f406d7362013bf63db3c9ba00290e1
Elvando J <elvandojhonny@gmail.com>
2026-06-15 13:07:16 +0800

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,7 +1,19 @@
 <?php
-$koneksi = mysqli_connect("localhost", "root", "", "db_peminjaman_ruang");
+
+$host = getenv('mysql.railway.internal');
+$user = getenv('root');
+$password = getenv('KaJYZOpVRPCbfWLaElIWjvsYWbSZUCpt');
+$database = getenv('railway');
+$port = getenv('3306');
+
+$koneksi = mysqli_connect(
+    $host,
+    $user,
+    $password,
+    $database,
+    $port
+);
 
 if (!$koneksi) {
     die("Koneksi gagal: " . mysqli_connect_error());
 }
-?>
